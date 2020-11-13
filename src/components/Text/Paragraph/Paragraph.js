23a9ae03e6baff01eed4d8e20588b250592a548a
Caio <caiopcmaia94@gmail.com>
2020-11-13 13:42:14 -0300

    commit on ignore-comma-on-word-comparisson

--- src/components/Text/Paragraph/Paragraph.js
+++ src/components/Text/Paragraph/Paragraph.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Wrapper } from './Wrapper'
-import PropTypes from 'prop-types'
-import { Word } from './Word'
+import React from "react";
+import { Wrapper } from "./Wrapper";
+import PropTypes from "prop-types";
+import { Word } from "./Word";
 
 export const Paragraph = ({
   words,
@@ -15,23 +15,24 @@ export const Paragraph = ({
       {paragraphIndex}
       {words.map((word, wordIndex) => (
         <Word
+          key={wordIndex}
           word={word}
-          marginLeft={wordIndex === 0 ? '2rem' : null}
+          marginLeft={wordIndex === 0 ? "2rem" : null}
           paragraphIndex={paragraphIndex}
           wordIndex={wordIndex}
           onClick={onWordClick}
           answerStatus={
             correctWords.includes(wordIndex)
-              ? 'correct'
+              ? "correct"
               : wrongWords.includes(wordIndex)
-              ? 'wrong'
-              : 'unanswered'
+              ? "wrong"
+              : "unanswered"
           }
         />
       ))}
     </Wrapper>
-  )
-}
+  );
+};
 
 Paragraph.propTypes = {
   words: PropTypes.arrayOf(PropTypes.string).isRequired,
@@ -39,4 +40,4 @@ Paragraph.propTypes = {
   onWordClick: PropTypes.func.isRequired,
   correctWords: PropTypes.arrayOf(PropTypes.number.isRequired),
   wrongWords: PropTypes.arrayOf(PropTypes.number.isRequired),
-}
+};
--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,67 +1,75 @@
-import React, { useState } from 'react'
-import { Wrapper } from './Wrapper'
-import PropTypes from 'prop-types'
-import { Paragraph } from './Paragraph'
+import React, { useState } from "react";
+import { Wrapper } from "./Wrapper";
+import PropTypes from "prop-types";
+import { Paragraph } from "./Paragraph";
 
-import { getParagraphWordsIndexes } from './getParagraphWordsIndexes'
-import { addOrRemoveFromArray } from './addOrRemoveFromArray'
+import { getParagraphWordsIndexes } from "./getParagraphWordsIndexes";
+import { addOrRemoveFromArray } from "./addOrRemoveFromArray";
 
 export const Text = ({ text, correctWords }) => {
-  const paragraphs = text.split('\n')
+  const paragraphs = text.split("\n");
 
-  const notEmpty = (text) => text.trim(text) !== ''
+  const notEmpty = (text) => text.trim(text) !== "";
   const paragraphsWords = paragraphs
     .filter(notEmpty)
-    .map((paragraph) => paragraph.trim().split(' '))
+    .map((paragraph) => paragraph.trim().split(" "));
 
-  const [correctClickedWords, setCorrectClickedWords] = useState([])
-  const [wrongClickedWords, setWrongClickedWords] = useState([])
+  const [correctClickedWords, setCorrectClickedWords] = useState([]);
+  const [wrongClickedWords, setWrongClickedWords] = useState([]);
 
-  const clickedWords = []
+  const clickedWords = [];
 
   const toggleWord = (_, { paragraphIndex, wordIndex }) => {
-    const clickedWordAddress = { paragraphIndex, wordIndex }
-    const word = paragraphsWords[paragraphIndex][wordIndex]
+    const clickedWordAddress = { paragraphIndex, wordIndex };
+    const wordWithPunctuation = paragraphsWords[paragraphIndex][wordIndex];
+    const punctuation = /[.,]/;
+    let word = {};
+    if (wordWithPunctuation.search(punctuation) !== -1) {
+      word = wordWithPunctuation.slice(0, -1);
+    } else {
+      word = wordWithPunctuation;
+    }
     const isCorrect = !!correctWords.find(
-      (correctWord) => correctWord.toLowerCase() === word.toLowerCase(),
-    )
+      (correctWord) => correctWord.toLowerCase() === word.toLowerCase()
+    );
     if (isCorrect) {
       addOrRemoveFromArray(
         correctClickedWords,
         clickedWordAddress,
-        setCorrectClickedWords,
-      )
+        setCorrectClickedWords
+      );
     } else {
       addOrRemoveFromArray(
         wrongClickedWords,
         clickedWordAddress,
-        setWrongClickedWords,
-      )
+        setWrongClickedWords
+      );
     }
-  }
+  };
 
   return (
     <Wrapper>
       {paragraphsWords.map((singleParagraphWords, paragraphIndex) => (
         <Paragraph
+          key={paragraphIndex}
           words={singleParagraphWords}
           paragraphIndex={paragraphIndex}
           onWordClick={toggleWord}
           correctWords={getParagraphWordsIndexes(
             correctClickedWords,
-            paragraphIndex,
+            paragraphIndex
           )}
           wrongWords={getParagraphWordsIndexes(
             wrongClickedWords,
-            paragraphIndex,
+            paragraphIndex
           )}
         />
       ))}
     </Wrapper>
-  )
-}
+  );
+};
 
 Text.propTypes = {
   text: PropTypes.string.isRequired,
   correctWords: PropTypes.arrayOf(PropTypes.string),
-}
+};
