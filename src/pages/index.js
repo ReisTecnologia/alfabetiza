88c4d563388caf5fb952ee4e35f111a10a76629d
Bruno Reis <bruno.p.reis@gmail.com>
2020-11-16 22:38:50 -0300

    clean seo file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,10 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { Layout } from '../components/Layout'
 import { Container } from '../components/Container'
-import { SEO } from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <Container>
-      <SEO
-        title="Home"
-        keywords={[`alfabetização`, `palavras`, `aprender a ler`]}
-      />
       <Link to="/letra/a">Letras</Link>
     </Container>
   </Layout>
