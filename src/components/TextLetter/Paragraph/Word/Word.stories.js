a8e973e3752ebadb460c4f1b39ab118c3065217f
Caio <caiopcmaia94@gmail.com>
2020-11-25 10:43:56 -0300

    Fixed word.stories of TextLetter

--- src/components/TextLetter/Paragraph/Word/Word.stories.js
+++ src/components/TextLetter/Paragraph/Word/Word.stories.js
@@ -19,14 +19,30 @@ BasicWord.args = {
 export const AnsweredWord = Template.bind({})
 AnsweredWord.args = {
   word: 'Alambrado',
-  correctLetters: [2, 4, 6],
-  wrongLetters: [0, 1, 3],
+  wordIndex: 0,
+  correctLetters: [
+    [0, 2],
+    [0, 4],
+  ],
+  wrongLetters: [
+    [0, 0],
+    [0, 1],
+    [0, 3],
+  ],
 }
 
 export const MarginLeft = Template.bind({})
 MarginLeft.args = {
   word: 'Alambrado',
-  correctLetters: [2, 4, 6],
-  wrongLetters: [0, 1, 3],
+  wordIndex: 0,
+  correctLetters: [
+    [0, 2],
+    [0, 4],
+  ],
+  wrongLetters: [
+    [0, 0],
+    [0, 1],
+    [0, 3],
+  ],
   marginLeft: '40px',
 }
