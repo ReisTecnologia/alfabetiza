31e8e2848bb3db752a9ead5566876ba1ee089fbf
Caio <caiopcmaia94@gmail.com>
2020-11-26 15:02:48 -0300

    changes to font size and spacing between words

--- src/components/TextWord/Paragraph/Word/Wrapper.js
+++ src/components/TextWord/Paragraph/Word/Wrapper.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-  margin-right: 5px;
-  font-size: 10vh;
+  margin-right: 2.2vh;
+  font-size: 6vh;
   margin-left: ${({ marginLeft }) => marginLeft || null};
   cursor: ${({ onClick }) => (onClick ? 'pointer' : null)};
   color: ${({ answerStatus }) => {
