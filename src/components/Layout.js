269b0211b3cc174a15fd3b580919f92133269450
Bruno Reis <bruno.p.reis@gmail.com>
2020-12-02 18:50:57 -0300

    small tweaks

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,22 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
 import { GlobalStyle } from './GlobalStyle'
 import { MainWrapper } from './MainWrapper'
 // import { Footer } from './Footer'
 
 export const Layout = ({ children }) => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-  )
   return (
     <>
       <GlobalStyle />
