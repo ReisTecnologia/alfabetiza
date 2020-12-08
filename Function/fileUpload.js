364defa354c2a8851631db4109ad5ae455624ddc
Caio <caiopcmaia94@gmail.com>
2020-12-08 15:40:29 -0300

    updated serverless configs and fileUpload function

--- Function/fileUpload.js
+++ Function/fileUpload.js
@@ -2,25 +2,32 @@ var express = require('express')
 var multer = require('multer')
 const AWS = require('aws-sdk')
 const serverless = require('serverless-http')
+const cors = require('cors')
 
 var upload = multer()
 var app = express()
 
 var s3 = new AWS.S3({
-  credentials: AWS.Credentials(
-    process.env.MY_AWS_ACCESS_KEY_ID,
-    process.env.MY_AWS_SECRET_ACCESS_KEY
-  ),
+  accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+})
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST,DELETE')
+  app.use(cors())
+  next()
 })
 
 app.post(
   '/.netlify/functions/fileUpload',
   upload.single('fileupload'),
   (req, res) => {
-    s3.putObject(
+    return s3.putObject(
       {
         Bucket: 'alfabetizaserver',
-        Key: req.body.filename,
+        // Key: req.body.filename,
+        Key: 'web app postman',
         Body: Buffer.from(req.file.buffer),
         ContentType: req.file.mimetype,
       },
@@ -29,8 +36,6 @@ app.post(
         console.log('Success')
       }
     )
-    res.send(req.file.mimetype)
-    res.end()
   }
 )
 module.exports.handler = serverless(app)
